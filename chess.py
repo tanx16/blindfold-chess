75b6e9360af019c1d2fecdde14dc6b626804a93c
alberttjin <albertjin@berkeley.edu>
2017-01-02 14:04:47 -0800

    Add files via upload

--- chess.py
+++ chess.py
@@ -99,7 +99,7 @@ class Chesspiece:
     def legalMove(self, board, new_col, new_row):
         #inside the board, no piece in destination that is your piece
         if new_col < 8 and new_col >= 0 and new_row < 8 and new_row >= 0:
-            return board.pieces[new_col][new_row] == None or board.pieces[new_col][new_row].color != self.color
+            return board.pieces[new_row][new_col] == None or board.pieces[new_row][new_col].color != self.color
 class Queen(Chesspiece):
     def legalMove(self, board, new_col, new_row, color):
         if Chesspiece.legalMove(board, new_col, new_row, color):
@@ -115,25 +115,27 @@ class King(Chesspiece):
         return False
 class Pawn(Chesspiece):
     def legalMove(self, board, new_col, new_row, color):
-        destination = board.pieces[new_col][new_row]
+        destination = board.pieces[new_row][new_col]
         if not Chesspiece.legalMove(self, board, new_col, new_row):
             return False
         if self.color == "white":
             if new_row == self.row + 1 and new_col == self.col and not destination:
                 return True
             else:
-                if not destination:
-                    return False
-                elif new_row == self.row + 1 and abs(new_col-self.col)==1 and destination.color != self.color:
-                    return True
+                if new_row == self.row + 1 and abs(new_col-self.col)==1:
+                    if not destination:
+                        return False
+                    else:
+                        return destination.color != self.color
         else:
             if new_row == self.row - 1 and new_col == self.col and not destination:
                 return True
             else:
-                if not destination:
-                    return False
-                elif new_row == self.row - 1 and abs(new_col-self.col)==1 and destination.color != self.color:
-                    return True
+                if new_row == self.row - 1 and abs(new_col-self.col)==1:
+                    if not destination:
+                        return False
+                    else:
+                        return destination.color != self.color
 class Knight(Chesspiece):
     def legalMove(self, board, new_col, new_row, color):
         if abs(new_row - self.row) == 2 and abs(new_col - self.col) == 1:
@@ -145,12 +147,17 @@ class Knight(Chesspiece):
 class Bishop(Chesspiece):
     def legalMove(self, board, new_col, new_row, color):
         destination = board.pieces[new_col][new_row]
+        print(destination)
         if not Chesspiece.legalMove(self, board, new_col, new_row):
+            print("original error")
             return False
         if not abs(self.row - new_row) == abs(self.col - new_col):
+            print("not straight line")
             return False
-        for i, j in zip(range(1, self.col - new.col), range(1, self.row - new.row)):
-            if board.pieces[self.col + i][self.row + j]:
+        index1 = (new_col - self.col) // abs(new_col - self.col)
+        index2 = (new_row - self.row) // abs(new_row - self.row)
+        for _ in range(abs(new_col - self.col)):
+            if board.pieces[self.row + index2][self.col + index1]:
                 return False
         if destination:
             if destination.color == self.color:
